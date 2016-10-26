94b0549465e7c35b149cdadea2ddd3de87558c8b
64b2b6d12b <64b2b6d12b@users.noreply.github.com>
2016-10-26 15:11:46 +1100

    Cleaned up code and added check for iso folder
    Some Cisco images store their contents in */iso/* hence added if check.

--- cscoboot.sh
+++ cscoboot.sh
@@ -2,13 +2,21 @@
 
 echo "Enter the filename of the non-bootable ISO e.g. UCSInstall_UCOS_10.5.1.10000-7.sgn.iso"
 read isoname
+
 sudo mkdir /mnt/iso
-mkdir /tmp/BootableISO
+sudo mkdir /tmp/BootableISO
 echo "Mounting non-bootable ISO..."
 sudo mount -o loop $isoname /mnt/iso
+
 echo "Copying data from ISO to tmp directory..."
 rsync -a /mnt/iso /tmp/BootableISO
+
+if [ -d /tmp/BootableISO/iso] ]; then
 cd /tmp/BootableISO/iso
+else
+cd /tmp/BootableISO
+fi
+
 echo "Creating new bootable ISO using data from tmp directory..."
 mkisofs -o /tmp/Bootable_$isoname -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -J -R .
 sudo umount -l /mnt/iso
